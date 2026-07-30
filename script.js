e2920c439f503eb0b854770d499354952062ee38
Anmol Gautam <anmolgautam1203.ag@gmail.com>
2026-07-30 21:46:07 +0530

    Add scrolling ground animation

--- script.js
+++ script.js
@@ -192,8 +192,7 @@ function drawBackground() {
   drawCloud(80 + cloudX, 80);
 drawCloud(280 + cloudX, 140);
 }
-
-    gameLoop();
+Loop();
 
 
 document.addEventListener("keydown", function (event) { 
